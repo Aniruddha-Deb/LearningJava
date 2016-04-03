9a39a97b5e610ca1c460eba940c3d83fff28da08
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2016-04-03 21:06:57 +0530

    added improvements to bookManager

--- src/com/sensei/bookMananger/LibraryManager.java
+++ src/com/sensei/bookMananger/LibraryManager.java
@@ -7,6 +7,15 @@ public class LibraryManager {
     
     private Library library = new Library() ;
     private Scanner scanner = new Scanner( System.in ) ;
+    public static String choice;
+
+    public String getChoiceForUserRetry() {
+        String buffer;
+        System.out.println( "Do you want to enter another book: " );
+        buffer = scanner.nextLine();
+        return buffer;
+    }
+    
     
     public void addBookToLibrary( Book book ) {
         library.addBook( book ) ;
@@ -16,6 +25,7 @@ public class LibraryManager {
         ArrayList<Book> books = library.getAllBooks() ;
         for( Book book : books ) {
             System.out.println( book ) ;
+            System.out.println( "------------------------------------" );
         }
     }
     
@@ -23,32 +33,52 @@ public class LibraryManager {
         
         Book book = null ;
         
-        System.out.println( "Would you like to enter data for another book? [y/n] " );
-        String choice = scanner.nextLine() ;
-        
-        if( choice.equals( "Y" ) || choice.equals( "y" ) ) {
-            book = new Book() ;
-            System.out.println( "Enter book name: " );
-            book.setBookName( scanner.nextLine() ) ;
-            System.out.println( "Enter book publisher: " );
-            book.setPublisher( scanner.nextLine() ) ;
-        }
-        else {
-           scanner.close() ; 
-        }
+        book = new Book() ;
+        System.out.println( "Enter book name: " );
+        book.setBookName( scanner.nextLine() ) ;
+        System.out.println( "Enter book publisher: " );
+        book.setPublisher( scanner.nextLine() ) ;
         
         return book ;
+    }
+    
+    public void printStartMenu() {
+        
+        System.out.println( "LibraryManager INDEV");
+        System.out.println( "================================================" );
+        System.out.println( "What would you like to do:" );
+        System.out.println( "    1) Display books in library" );
+        System.out.println( "    2) Add new book to library" );
+        System.out.println( "    3) Exit LibraryManager" );
+        System.out.println( "================================================" );
+        System.out.println( "Wating on choice:> " );
+        choice = scanner.nextLine();
     }
     
     public static void main( String[] args ) {
        
         LibraryManager manager = new LibraryManager() ;
-        Book book = manager.createNewBook() ;
-        while( book != null ) {
-            manager.addBookToLibrary( book ) ;
-            book = manager.createNewBook() ;
-        }
+        Book book;
         
-        manager.listBooksInLibrary() ;
+        do {
+            manager.printStartMenu();
+            if( choice.equals( "1" ) ) {
+                manager.listBooksInLibrary();
+                System.out.println( "========================================" );
+                System.out.println( "" );
+            }
+            else if( choice.equals( "2" )) {
+                String input;
+                do {
+                    book = manager.createNewBook();
+                    manager.addBookToLibrary( book );
+                    input = manager.getChoiceForUserRetry();
+                } while( input.equals( "y" ) || input.equals( "Y" ) ); 
+            }
+            else if( choice.equals( "3" ) ) {
+                new Scanner( System.in ).close();
+                System.exit( -1 );
+            }
+        } while ( true );
     }
 }
