db923a2e6c81c8312f3b56cc6bab7b38cde74f43
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2016-04-26 13:49:57 +0530

    added instructions and the playing board is now drawn by the program

--- src/com/sensei/rover/Rover.java
+++ src/com/sensei/rover/Rover.java
@@ -19,12 +19,34 @@ public class Rover {
 		choice = scanner.next();
 	}
 	
-	public static void playGame() {
+	public static void drawBoard() {
 		
+		System.out.println( "" );
+		System.out.println( "" );
+		
+		for( int i=0; i<5; i++ ) {
+			for( int j=0; j<8; j++ ) {
+				System.out.print( "\u2022" + "        " );
+			}
+			System.out.println( "" );
+			System.out.println( "" );
+			System.out.println( "" );
+		}
+	}
+	
+	public static void playGame() {
+		drawBoard();
 	}
 	
 	public static void displayInstructions() {
-		
+		System.out.println( "You serve as the captain of a Rover operating in Antarctica." );
+		System.out.println( "Your orders are to tap into the deepest trench to discover fossils." );
+		System.out.println( "" );
+		System.out.println( "The board contains 40 trenches with the maximum X coordinate being 7 and the " );
+		System.out.println( "maximum Y coordinate being 4. You start at 0,0. Type the coordinate to travel " );
+		System.out.println( "to in the prompt. Once there, you can see the depths of the neighbouring " );
+		System.out.println( "trenches. Once you are sure that the trench underneath is the deepest, hit Enter" );
+		System.out.println( "to dig. If it is the deepest, you are successful. If not, better luck next time." );
 	}
 	
 	public static void exit() {
