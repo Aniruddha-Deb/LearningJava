eae6d51b5de13453c722904d1b4994fc53f25f66
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-09-14 17:31:16 +0530

    some experiments with fonts

--- JavaFX/src/com/sensei/javafx/hellofx/HelloFX.java
+++ JavaFX/src/com/sensei/javafx/hellofx/HelloFX.java
@@ -2,7 +2,14 @@ package com.sensei.javafx.hellofx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Effect;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,11 +19,28 @@ public class HelloFX extends Application{
 	public void start( Stage primaryStage ) throws Exception {
 		
 		VBox root = new VBox();
-		Text helloFX = new Text();
-		helloFX.setText( "Hello FX!" );
-		root.getChildren().add( helloFX );
+		Canvas c = new Canvas( 500, 500 );
+		root.getChildren().add( c );
+		GraphicsContext g = c.getGraphicsContext2D();
 		
-		Scene s = new Scene( root, 300, 50 );
+		Text t = new Text( "C" );
+		t.setFont( new Font( "Helvetica", 128 ) );
+		
+		double rem = Math.rint( t.getLayoutBounds().getHeight() )-4;
+		double rin = Math.rint( t.getLayoutBounds().getWidth() );
+				
+		System.out.println( rem + " " + rin );
+		
+		g.setFill( Color.BLACK );
+		g.setFont( new Font( "Helvetica", 128 ) );
+		g.strokeLine( 250, 250, 250, 250 );
+		g.fillText( "C", 250-(rin/2), 250+(rem/2) );
+		g.setStroke( Color.LIGHTGRAY );
+		g.setLineDashes( 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 );
+		g.strokeArc( 250-10, 250-10, 20, 20, 0, 360, ArcType.CHORD );
+		g.strokeArc( 250-25, 250-25, 50, 50, 0, 360, ArcType.CHORD );
+		
+		Scene s = new Scene( root, 500, 500 );
 		
 		primaryStage.setTitle( "Hello FX!" );
 		primaryStage.setScene( s );
