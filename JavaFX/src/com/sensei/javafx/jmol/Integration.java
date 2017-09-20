c842ecf5dadcf2699834dacf50696145d65657e9
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-09-20 18:25:23 +0530

    experimented with jmol

---
+++ JavaFX/src/com/sensei/javafx/jmol/Integration.java
@@ -0,0 +1,12 @@
+package com.sensei.javafx.jmol;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Integration extends Application {
+
+	@Override
+	public void start( Stage primaryStage ) throws Exception {
+		
+	}
+}
